067e93334eacfd8c8707f31ebecf1285e4fb4ecd
Joseph-Villegas <jovillegas@csumb.edu>
2020-10-01 13:52:41 -0700

    Added DetailActivity

--- app/src/main/java/edu/csumb/vill4031/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/edu/csumb/vill4031/flixster/adapters/MovieAdapter.java
@@ -1,20 +1,26 @@
 package edu.csumb.vill4031.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+import edu.csumb.vill4031.flixster.DetailActivity;
 import edu.csumb.vill4031.flixster.R;
 import edu.csumb.vill4031.flixster.models.Movie;
 
@@ -53,6 +59,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -63,9 +70,10 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
 
@@ -73,6 +81,17 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
             int orientation = context.getResources().getConfiguration().orientation;
             String imageUrl = (orientation == Configuration.ORIENTATION_LANDSCAPE) ? movie.getBackdropPath() : movie.getPosterPath();
             Glide.with(context).load(imageUrl).into(ivPoster);
+
+            // Register click listener on the whole row
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Navigate to a new activity on tap
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
